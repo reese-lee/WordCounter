48a5173523e4e3c2ba442674341b28c558d18be2
Reese Lee <reese.lee.cy@gmail.com>
2019-04-26 13:19:53 -0700

    add test method to return user-inputted word in lowercase

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -1,17 +1,49 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WordCounter.Models;
+using System;
+using System.Collections.Generic;
 
 namespace WordCounter.Tests
 {
   [TestClass]
-  public class WordTest
+  public class RepeatCounterTests
   {
     [TestMethod]
-    public void UserInput_CreatesInstanceOfWordandSentence_Strings()
+    public void RepeatCounterConstructor_CreatesInstanceOfObject_Strings()
     {
-      RepeatCounter userCounter = new RepeatCounter("jelly", "The jelly beans are good");
+      RepeatCounter userCounter = new RepeatCounter("jelly");
       Assert.AreEqual(typeof(RepeatCounter), userCounter.GetType());
     }
+
+    [TestMethod]
+    public void ReturnUserWord_UserWordIsReturned_String()
+    {
+      string word = "jelly";
+      RepeatCounter userCounter = new RepeatCounter("JELLY");
+      string wordCheck = userCounter.GetWord();
+      Assert.AreEqual(word, wordCheck);
+    }
+
+    // [TestMethod]
+    // public void GetsSentence_ReturnsLetterCharactersOnly_String()
+    // {
+    //   RepeatCounter userCounter = new RepeatCounter("jelly", "The jelly beans are good");
+    //   string userInput = "choco sta1ns";
+    //   string lettersOnly = userCounter.GetLetters(userInput);
+    //   Assert.AreEqual("choco", lettersOnly);
+    // }
+
+    // [TestMethod]
+    // public void CheckWord_CheckIfWordRepeats_True()
+    // {
+    //
+    // }
+    //
+    // [TestMethod]
+    // public void CheckWordOccurence_CheckNumberOfTimesWordAppears_Int()
+    // {
+    //
+    // }
 
   }
 }
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -1,19 +1,45 @@
 // using System;
 // using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace WordCounter.Models
 {
   public class RepeatCounter
   {
     private string _word;
-    private string _sentence;
-    private int _result;
+    // private string _sentence;
+    // private int _result;
 
-    public RepeatCounter(string word, string sentence)
+    public RepeatCounter(string userInput)
     {
-      _word = word;
-      _sentence = sentence;
+      _word = userInput.ToLower();
     }
+
+    public string GetWord()
+    {
+      return _word.ToLower();
+    }
+
+    // public int GetResult()
+    // {
+    //   return _result;
+    // }
+
+    public void SetSearchWord(string newWord)
+    {
+      _word = newWord.ToLower();
+    }
+
+    // public int CheckWords()
+    // {
+    //   string userWord = word;
+    //   char[] userArray = userWord.ToCharArray();
+    //   for (index i = 0; i < userArray.Count; i++)
+    //   {
+    //
+    //   }
+    // }
+
 
 
   }
